533de529ddc9b1121811d581569e1eeb661e3bea
Krystian Ruczyński <krystian.ruczynski@student.wat.edu.pl>
2021-11-21 20:52:34 +0100

    Implementacja autouzupełniania adresu email w przypadku zalogowanego użytkownika

--- SUB/Pages/Contact/Create.cshtml.cs
+++ SUB/Pages/Contact/Create.cshtml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using SUB.Data;
 using SUB.Models;
 
