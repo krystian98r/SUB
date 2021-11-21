a2164526ebfbec68f4c287d3a3c3100de792fb48
Krystian Ruczyński <krystian.ruczynski@student.wat.edu.pl>
2021-11-21 20:28:06 +0100

    Rozszerzenie funkcjonalności

--- SUB/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SUB/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -45,6 +45,10 @@ namespace SUB.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+            [Required(ErrorMessage = "Pole jest wymagane")]
+            [Display(Name = "Nazwa użytkownika")]
+            public string Username { get; set; }
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -53,12 +57,12 @@ namespace SUB.Areas.Identity.Pages.Account
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Hasło")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Display(Name = "Potwierdź hasło")]
+            [Compare("Password", ErrorMessage = "Hasło i potwierdzienie hasła nie są takie same.")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -74,7 +78,7 @@ namespace SUB.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new IdentityUser { UserName = Input.Username, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
